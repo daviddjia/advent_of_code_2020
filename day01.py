12ea18431b351d16cc175472b12bb23d39262702
David Jia <david.d.jia@gmail.com>
2020-12-01 00:15:07 -0500

    Done with day 01!

--- day01.py
+++ day01.py
@@ -2,4 +2,15 @@
 
 f = open('day01_input.txt', 'r')
 
-lines = f.readlines()
+lines = [int(i) for i in f.readlines()]
+
+for i in range(0, len(lines)):
+    for j in range(i+1, len(lines)):
+        if lines[i]+lines[j] == 2020:
+            print(lines[i]*lines[j])
+
+for i in range(0, len(lines)):
+    for j in range(i+1, len(lines)):
+        for k in range(j+1, len(lines)):
+            if lines[i]+lines[j]+lines[k] == 2020:
+                print(lines[i]*lines[j]*lines[k])
