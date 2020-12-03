cf2292d959601e22b2e2cb046d0c31e16a46195f
David Jia <david.d.jia@gmail.com>
2020-12-03 01:20:37 -0500

    Days 2 and 3

--- day02.py
+++ day02.py
@@ -3,3 +3,29 @@
 f = open('day02_input.txt', 'r')
 
 lines = f.readlines()
+
+total_valid = 0
+for l in lines:
+    [count_range, char, pw] = l.split(' ')
+    count_range = [int(cr) for cr in count_range.split('-')]
+    char = char[:-1]
+
+    letter_count = 0
+    for p in pw:
+        if p == char:
+            letter_count += 1
+    if letter_count >= count_range[0] and letter_count <= count_range[1]:
+        total_valid += 1
+
+print(total_valid)
+
+total_valid = 0
+for l in lines:
+    [indexes, char, pw] = l.split(' ')
+    indexes = [int(i) for i in indexes.split('-')]
+    char = char[:-1]
+
+    if (pw[indexes[0]-1] == char) != (pw[indexes[1]-1] == char):
+        total_valid += 1
+
+print(total_valid)
--- day03.py
+++ day03.py
@@ -2,4 +2,29 @@
 
 f = open('day03_input.txt', 'r')
 
-lines = f.readlines()
+lines = [l.strip() for l in f.readlines()]
+
+count = 0
+j = 0
+for i, line in enumerate(lines):
+    if line[j] == '#':
+        count += 1
+    j = (j+3)%(len(line))
+print(count)
+
+slopes = [(1, 1), (1, 3), (1, 5), (1, 7), (2, 1)]
+counts = []
+for slope in slopes:
+    count = 0
+    i = 0
+    j = 0
+    while i < len(lines):
+        if lines[i][j] == '#':
+            count += 1
+        i += slope[0]
+        j = (j+slope[1])%(len(lines[0]))
+    counts.append(count)
+product = 1
+for c in counts:
+    product *= c
+print(product)
