0e24d68426649f201e3f29d7cdeb3eab76897aa4
David Jia <david.d.jia@gmail.com>
2020-12-05 16:47:53 -0500

    Day 5 in the books!

--- day05.py
+++ day05.py
@@ -1,5 +1,28 @@
 #!/bin/env/python
 
-f = open('day04_input.txt', 'r')
+f = open('day05_input.txt', 'r')
 
-lines = [line for lines in f.readlines()]
+lines = [line.strip() for line in f.readlines()]
+
+seat_ids = []
+for line in lines:
+    row_code = line[0:7]
+    col_code = line[7:]
+    row = 0
+    col = 0
+
+    for i, char in enumerate(row_code[::-1]):
+        if char == 'B':
+            row += 1 << i
+    for i, char in enumerate(col_code[::-1]):
+        if char == 'R':
+            col += 1 << i
+
+    seat_id = row*8+col
+    seat_ids.append(seat_id)
+print(max(seat_ids))
+
+sorted_seat_ids = sorted(seat_ids)
+for i, seat_id in enumerate(sorted_seat_ids[1:]):
+    if seat_id-1 != sorted_seat_ids[i]:
+        print(str(seat_id-1))
