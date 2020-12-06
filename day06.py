fa39f81ebde493ad29cc91a9a835a9e1dbf8dda6
David Jia <david.d.jia@gmail.com>
2020-12-06 00:34:13 -0500

    Ugh, started Day 6 like 10 mins late. Super easy day though

--- day06.py
+++ day06.py
@@ -1,5 +1,42 @@
 #!/bin/env/python
 
-f = open('day04_input.txt', 'r')
+f = open('day06_input.txt', 'r')
 
 lines = [line.strip() for line in f.readlines()]
+
+groups = []
+group = ''
+for line in lines:
+    if line == '':
+        groups.append(group.strip())
+        group = ''
+    else:
+        group += line
+groups.append(group.strip())
+
+count = 0
+for group in groups:
+    count += len(list(set(list(group))))
+print(count)
+
+groups = []
+group = []
+for line in lines:
+    if line == '':
+        groups.append(group)
+        group = []
+    else:
+        group.append(line)
+groups.append(group)
+
+count = 0
+for group in groups:
+    intersection = set()
+    for i, person in enumerate(group):
+        person_set = set(list(person))
+        if i == 0:
+            intersection = person_set
+        else:
+            intersection = intersection.intersection(person_set)
+    count += len(list(intersection))
+print(count)
