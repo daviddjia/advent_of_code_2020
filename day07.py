00125b80cef87291bf3988ad38d88509820c24bc
David Jia <david.d.jia@gmail.com>
2020-12-08 00:42:18 -0500

    First week is over! Day 7 and 8 complete

--- day07.py
+++ day07.py
@@ -1,5 +1,81 @@
 #!/bin/env/python
 
-f = open('day07_input.txt', 'r')
+import re
 
+f = open('day07_input.txt', 'r')
 lines = [line.strip() for line in f.readlines()]
+
+class Bag(object):
+    def __init__(self, name):
+        self.name = name
+        self.inner_bags = set()
+        self.outer_bags = set()
+        self.inner_bag_count = {}
+
+    def __repr__(self):
+        return self.name
+
+    def __str__(self):
+        return self.name
+
+bag_map = {}
+
+def add_rule(bag_map, inner_bag, outer_bag):
+    if inner_bag in bag_map:
+        bag_map[inner_bag].add(outer_bag)
+    else:
+        bag_map[inner_bag] = {outer_bag}
+
+    if outer_bag in bag_map:
+        for outer_outer_bag in bag_map[outer_bag]:
+            add_rule(bag_map, inner_bag, outer_outer_bag)
+
+for line in lines:
+    regex = re.match('^(.*) bags contain (.*)\.$', line)
+    outer_bag = regex.group(1).strip()
+    if outer_bag not in bag_map:
+        bag_map[outer_bag] = Bag(outer_bag)
+    ob = bag_map[outer_bag]
+
+    inner_bags = [
+        inner_bag.strip()
+        for inner_bag in regex.group(2).strip().split(',')
+    ]
+    if inner_bags[0] == 'no other bags':
+        bag_map[inner_bags[0]] = Bag(inner_bags[0])
+    else:
+        for inner_bag in inner_bags:
+            regex = re.match('^(\d+) (.*) bags*$', inner_bag)
+            count = regex.group(1)
+            inner_bag_formatted = regex.group(2)
+            if inner_bag_formatted not in bag_map:
+                bag_map[inner_bag_formatted] = Bag(inner_bag_formatted)
+            ib = bag_map[inner_bag_formatted]
+            ob.inner_bags.add(ib)
+            ib.outer_bags.add(ob)
+            ob.inner_bag_count[ib] = int(count)
+
+def get_outer_bags(bag):
+    if bag.outer_bags:
+        names = set([ob.name for ob in list(bag.outer_bags)])
+        for outer_bag in list(bag.outer_bags):
+            names = names.union(get_outer_bags(outer_bag))
+        return names
+    else:
+        return {bag.name}
+
+unique_bag_names = get_outer_bags(bag_map['shiny gold'])
+print(len(list(unique_bag_names)))
+
+def get_inner_bag_count(bag):
+    if not bag.inner_bag_count:
+        return 1
+
+    count = 0
+    for ib in bag.inner_bag_count:
+        if ib.inner_bag_count:
+            count += bag.inner_bag_count[ib]
+        count += bag.inner_bag_count[ib] * get_inner_bag_count(ib)
+    return count
+
+print(get_inner_bag_count(bag_map['shiny gold']))
--- day08.py
+++ day08.py
@@ -1,5 +1,56 @@
 #!/bin/env/python
 
 f = open('day08_input.txt', 'r')
-
 lines = [line.strip() for line in f.readlines()]
+
+run_lines = set()
+acc = 0
+pointer = 0
+while pointer not in run_lines:
+    line = lines[pointer]
+    instruction, param = line.split()
+    param = int(param)
+
+    run_lines.add(pointer)
+
+    if instruction == 'acc':
+        acc += param
+        pointer += 1
+    elif instruction == 'jmp':
+        pointer += param
+    else:
+        pointer += 1
+print(acc)
+
+for change_from, change_to in [('jmp', 'nop'), ('nop', 'jmp')]:
+    error_lines = [
+        i
+        for i, line in enumerate(lines)
+        if line.split()[0] == change_from]
+
+    for error_line_pointer in error_lines:
+        error_line = lines[error_line_pointer]
+
+        run_lines = set()
+        acc = 0
+        pointer = 0
+        while pointer not in run_lines and pointer != len(lines):
+            line = lines[pointer]
+            instruction, param = line.split()
+            param = int(param)
+            if pointer == error_line_pointer:
+                instruction = change_to
+
+            run_lines.add(pointer)
+
+            if instruction == 'acc':
+                acc += param
+                pointer += 1
+            elif instruction == 'jmp':
+                pointer += param
+            else:
+                pointer += 1
+
+        if pointer == len(lines):
+            print(acc)
+            break
