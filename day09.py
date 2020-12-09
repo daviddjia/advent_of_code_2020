6ff0681630f69cd888b30e5d15b22de1955aad7c
David Jia <david.d.jia@gmail.com>
2020-12-09 00:41:53 -0500

    Omg, easy Day 9, but typos and misreading-the-effing-prompt slowed me down... :(

--- day09.py
+++ day09.py
@@ -1,5 +1,34 @@
 #!/bin/env/python
 
-f = open('day09_input.txt', 'r')
+from collections import deque
+from itertools import combinations
 
-lines = [line.strip() for line in f.readlines()]
+f = open('day09_input.txt', 'r')
+nums = [int(line.strip()) for line in f.readlines()]
+
+preamble = deque(nums[:25])
+for num in nums[25:]:
+    found_sum = False
+    for (x, y) in combinations(preamble, 2):
+        if x == y:
+            break
+        if x+y == num:
+            found_sum = True
+            break
+
+    if not found_sum:
+        break
+
+    preamble.popleft()
+    preamble.append(num)
+print(num)
+
+result = num
+for i, num in enumerate(nums):
+    sum = num
+    for j, x in enumerate(nums[i+1:]):
+        sum += x
+        if sum == result:
+            print(min(nums[i:i+j+2]) + max(nums[i:i+j+2]))
+        elif sum > result:
+            break
