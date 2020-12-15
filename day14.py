ab0bf2d183ec51791f7bc4c90874a2c8a59b91ab
David Jia <david.d.jia@gmail.com>
2020-12-15 00:42:45 -0500

    Fixed up Day 14 to be a bit cleaner

--- day14.py
+++ day14.py
@@ -6,42 +6,36 @@ import math
 f = open('day14_input.txt', 'r')
 lines = [line.strip() for line in f.readlines()]
 
+def parse_memory_instruction(line):
+    return tuple(int(i) for i in re.findall('(\d+)', line))
+
 memory = {}
 for line in lines:
-    if line[0:4] == 'mask':
+    if 'mask' in line:
         mask = line.split()[2]
         zero_mask = int(mask.replace('X', '1'), 2)
         one_mask = int(mask.replace('X', '0'), 2)
     else:
-        regex = re.match('mem\[(\d+)\]\s+=\s+(\d+)', line)
-        address, value = int(regex.group(1)), int(regex.group(2))
+        address, value = parse_memory_instruction(line)
         value = (value & zero_mask) | one_mask
         memory[address] = value
 print(sum(memory.values()))
 
 memory = {}
 for line in lines:
-    if line[0:4] == 'mask':
+    if 'mask' in line:
         masks = []
         mask = line.split()[2]
-        x_indices = []
-        for i, char in enumerate(mask):
-            if char == 'X':
-                x_indices.append(i)
+        x_indices = [i for i, char in enumerate(mask) if char == 'X']
         for i in range(int(math.pow(2, len(x_indices)))):
-            bin_num = bin(i)[2:]
-            if len(bin_num) != len(x_indices):
-                bin_num = bin_num.rjust(len(x_indices), '0')
+            bin_num = bin(i)[2:].zfill(len(x_indices))
             zero_mask_list, one_mask_list = list(mask.replace('0', '1')), list(mask)
-            one_mask_list = list(mask)
             for j, index in enumerate(x_indices):
                 zero_mask_list[index] = one_mask_list[index] = bin_num[j]
             zero_mask, one_mask = ''.join(zero_mask_list), ''.join(one_mask_list)
             masks.append((int(zero_mask, 2), int(one_mask, 2)))
     else:
-        regex = re.match('mem\[(\d+)\]\s+=\s+(\d+)', line)
-        address, value = int(regex.group(1)), int(regex.group(2))
+        address, value = parse_memory_instruction(line)
         for zero_mask, one_mask in masks:
             memory[(address & zero_mask) | one_mask] = value
 print(sum(memory.values()))
-
