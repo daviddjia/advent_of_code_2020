2daaffd3d1ba60b2a67d9c807fe351171747cdad
David Jia <david.d.jia@gmail.com>
2020-12-25 00:38:37 -0500

    Need a that was easy button for Day 25
    It's over!!!

--- day25.py
+++ day25.py
@@ -1,4 +1,27 @@
 #!/bin/env/python
 
-f = open('day25_input.txt', 'r')
-lines = [line.strip() for line in f.readlines()]
+def parse_input():
+    f = open('day25_input.txt', 'r')
+    return [int(line.strip()) for line in f.readlines()]
+
+def calculate_loop_size(key):
+    value = 1
+    subject_num = 7
+    loop_size = 0
+    while value != key:
+        value *= subject_num
+        value = value % 20201227
+        loop_size += 1
+    return loop_size
+
+def transform(subject_num, loop_size):
+    value = 1
+    for i in range(loop_size):
+        value *= subject_num
+        value = value % 20201227
+    return value
+
+card_pub_key, door_pub_key = parse_input()
+
+card_loop_size = calculate_loop_size(card_pub_key)
+print(transform(door_pub_key, card_loop_size))
